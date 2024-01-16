561db4fc172b934a97a51bb3fb9b544b796a832b
mwangiowen <owen31588@gmail.com>
2024-01-16 21:10:27 +0300

    update models.cpython-311.pyc, app.py and models.py

--- python-code-challenge-superheroes/code-challenge/app/app.py
+++ python-code-challenge-superheroes/code-challenge/app/app.py
@@ -5,21 +5,31 @@ from flask_migrate import Migrate
 
 from models import db, Hero
 import os
+
 abs_path = os.getcwd()
 
-db_path = f'{abs_path}/db/heroes.db'
+db_path = f"{abs_path}/db/heroes.db"
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return ''
+    return ""
 
 
-if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+@app.route("/add-hero")
+def add_hero():
+    hero = Hero(name="Clark Kent", super_name="Superman")
+    db.session.add(hero)
+    db.session.commit()
+    return "Hero added!"
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
