fca4fa7a38d92a05bf7b1c69d10fe6efda3de7e6
mwangiowen <owen31588@gmail.com>
2024-01-16 22:25:21 +0300

    update 6 files

--- python-code-challenge-superheroes/code-challenge/app/app.py
+++ python-code-challenge-superheroes/code-challenge/app/app.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
+
 
 from models import db, Hero
 import os
 
 abs_path = os.getcwd()
+all_heroes = [
+    {"name": "Kamala Khan", "super_name": "Ms. Marvel"},
+    {"name": "Doreen Green", "super_name": "Squirrel Girl"},
+    {"name": "Gwen Stacy", "super_name": "Spider-Gwen"},
+    {"name": "Janet Van Dyne", "super_name": "The Wasp"},
+    {"name": "Wanda Maximoff", "super_name": "Scarlet Witch"},
+    {"name": "Carol Danvers", "super_name": "Captain Marvel"},
+    {"name": "Jean Grey", "super_name": "Dark Phoenix"},
+    {"name": "Ororo Munroe", "super_name": "Storm"},
+    {"name": "Kitty Pryde", "super_name": "Shadowcat"},
+    {"name": "Elektra Natchios", "super_name": "Elektra"},
+]
 
 db_path = f"{abs_path}/db/heroes.db"
 app = Flask(__name__)
@@ -23,12 +36,9 @@ def home():
     return "This is the home page bro!"
 
 
-@app.route("/add-hero")
-def add_hero():
-    hero = Hero(name="Clark Kent", super_name="Superman")
-    db.session.add(hero)
-    db.session.commit()
-    return "Hero added!"
+@app.route("/heroes")
+def get_heroes():
+    return jsonify(all_heroes)
 
 
 if __name__ == "__main__":
